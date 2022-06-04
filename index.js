92814775ff375b002288f39d433a67b4c9f486bd
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-06-04 12:50:18 +0600

    1

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId, Transaction } = require('mongodb');
 require('dotenv').config()
-const jwt = require('jsonwebtoken')
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 const app = express();
